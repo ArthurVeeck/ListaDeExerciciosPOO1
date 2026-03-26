796f6e9bc9cb77a3cb9c54c92a7fc99958cb48a8
ArthurVeeck <arthur.silva.veeck@gmail.com>
2026-03-26 16:19:16 -0300

    Exercicio15

---
+++ Exercicio15/src/exercicio15/Hamburguer.java
@@ -0,0 +1,51 @@
+
+package exercicio15;
+
+
+public class Hamburguer extends ItemCardapio {
+    private boolean artesanal;
+    private int quantidadeAdicionais;
+    private double valorAdicional;
+
+    public Hamburguer(String nome, double preco, String descricao, boolean artesanal,
+            int quantidadeAdicionais, double valorAdicional) {
+        super(nome, preco, descricao);
+        this.artesanal = artesanal;
+        this.quantidadeAdicionais = quantidadeAdicionais;
+        this.valorAdicional = valorAdicional;
+    }
+
+    public boolean GetArtesanal() {
+        return artesanal;
+    }
+
+    public void setArtesanal(boolean artesanal) {
+        this.artesanal = artesanal;
+    }
+
+    public int getQuantidadeAdicionais() {
+        return quantidadeAdicionais;
+    }
+
+    public void setQuantidadeAdicionais(int quantidadeAdicionais) {
+        this.quantidadeAdicionais = quantidadeAdicionais;
+    }
+
+    public double getValorAdicional() {
+        return valorAdicional;
+    }
+
+    public void setValorAdicional(double valorAdicional) {
+        this.valorAdicional = valorAdicional;
+    }
+    
+    @Override
+    public void preparar() {
+        System.out.println("Montando hambúrguer " + getNome());
+    }
+
+    @Override
+    public double calcularPreco() {
+        return getPreco() + (getQuantidadeAdicionais() * getValorAdicional());
+    }
+}

b21ac980dacf175f18c344334f62812be2b0ccc8
ArthurVeeck <arthur.silva.veeck@gmail.com>
2026-03-26 16:36:47 -0300

    Exercicio17

---
+++ Exercicio17/src/exercicio17/Exercicio17.java
@@ -0,0 +1,26 @@
+
+package exercicio17;
+
+import java.util.ArrayList;
+
+
+public class Exercicio17 {
+
+    
+    public static void main(String[] args) {
+        ArrayList<ItemBiblioteca> itens = new ArrayList<>();
+
+        itens.add(new Livro("Java Básico", "L01", true, "Autor X", 200, 7));
+        itens.add(new Revista("Tech News", "R01", true, 12, "Tecnologia", 3));
+        itens.add(new Jornal("Jornal Diário", "J01", true, "26/03/2026", "SP", true));
+        itens.add(new DVD("Filme Ação", "D01", true, "Ação", 120, 2.5));
+
+        for (ItemBiblioteca item : itens) {
+            item.exibir();
+            item.emprestar(); 
+            item.devolver();
+            System.out.println("");
+        }
+    }
+    
+}
